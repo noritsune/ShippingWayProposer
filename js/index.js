fa3e6c4386cf84e9c19ba2556047e0619ed31a8a
noritsune <bill.gatess4037@gmail.com>
2020-11-28 23:19:29 +0900

    Fix: 必須項目を埋めていなくても処理を進めてしまうバグを修正

--- js/index.js
+++ js/index.js
@@ -4,5 +4,22 @@ function GetInput() {
     let depth = document.getElementById("depth").value;
     let height = document.getElementById("height").value;
 
+    if(HasEmptyForm(new Array(weight, width, depth, height))) {
+        window . alert( '必須項目は全て入力して下さい。' );
+        return false;
+    }
+
     console.log("重さ：" + weight + ", 幅：" + width + ", 奥行き：" + depth + ", 高さ：" + height);
+
+    //TODO: 配送方法を表示する
+}
+
+function HasEmptyForm(inputs){
+    for (let i = 0; i < inputs.length; i++) {
+        if(inputs[i] == "") {
+            return true;
+        }
+    }
+
+    return false;
 }
