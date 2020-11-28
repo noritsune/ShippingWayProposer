ffaca8f06419ca0ea1ded7b563b9a08fea6b9eb6
noritsune <bill.gatess4037@gmail.com>
2020-11-28 22:41:05 +0900

    Add: ボタン押下時に入力値を取得する

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function GetInput() {
+    var weight = document.getElementById("weight").value;
+    var width = document.getElementById("width").value;
+    var depth = document.getElementById("depth").value;
+    var height = document.getElementById("height").value;
+
+    
+}
