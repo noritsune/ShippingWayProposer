43e3cbc341eee611dc6edbc0256dc006620e7a4a
noritsune <bill.gatess4037@gmail.com>
2020-11-28 23:47:16 +0900

    Change: 入力フォームに空欄があるとボタンを押せなくする

--- js/index.js
+++ js/index.js
@@ -1,25 +1,34 @@
-function GetInput() {
-    let weight = document.getElementById("weight").value;
-    let width = document.getElementById("width").value;
-    let depth = document.getElementById("depth").value;
-    let height = document.getElementById("height").value;
-
-    if(HasEmptyForm(new Array(weight, width, depth, height))) {
-        window . alert( '必須項目は全て入力して下さい。' );
-        return false;
+//入力フォームへの入力値を全て読んでオブジェクトで返す
+function GetAllInputs() {
+    return {
+        weight: document.getElementById("weight").value,
+        width: document.getElementById("width").value,
+        depth: document.getElementById("depth").value,
+        height: document.getElementById("height").value,
     }
-
-    console.log("重さ：" + weight + ", 幅：" + width + ", 奥行き：" + depth + ", 高さ：" + height);
-
-    //TODO: 配送方法を表示する
 }
 
-function HasEmptyForm(inputs){
-    for (let i = 0; i < inputs.length; i++) {
-        if(inputs[i] == "") {
+//空の入力項目があればtrueを返す
+function HasEmptyForm(input){
+    for(let key in input) {
+        if(input[key] == "") {
             return true;
         }
     }
 
     return false;
+}
+
+//入力フォームが全て埋まっていればsubmitButtonを有効化する
+function ButtonMakeEnable() {
+    document.getElementById("submitButton").disabled = HasEmptyForm(GetAllInputs());
+}
+
+//配送方法を表示する
+function ShowShippingWay() {
+    let input = GetAllInputs();
+
+    console.log("重さ：" + input.weight + ", 幅：" + input.width + ", 奥行き：" + input.depth + ", 高さ：" + input.height);
+
+    //TODO: 配送方法を表示する
 }
